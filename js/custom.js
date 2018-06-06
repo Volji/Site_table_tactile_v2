c578d97dccd4b82d2b84455caed48e354c82506b
volji <chipset@laposte.net>
2018-06-06 09:36:53 +0200

    First Commit
    local repo to github

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+// Typed Plugin
+typed = new Typed('.type', {
+		strings: ["PROJET.", "RÉUSSITE.", "SUCCÈS."], /* Here Type Your Title */
+		typeSpeed: 100,
+		loop: true,
+		backDelay: 1200,
+		backSpeed: 20
+});
